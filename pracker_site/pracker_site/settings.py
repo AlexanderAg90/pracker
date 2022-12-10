b29837cb396f024b7b89eb393d81b40d3ec7f5a2
Alexander Agapkin <alexander.agapkin@gmail.com>
2022-12-10 16:53:20 +0300

    Fix static files settings

--- pracker_site/pracker_site/settings.py
+++ pracker_site/pracker_site/settings.py
@@ -125,13 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-print('static root: ', STATIC_ROOT)
-print('static files dirs: ', STATICFILES_DIRS)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
